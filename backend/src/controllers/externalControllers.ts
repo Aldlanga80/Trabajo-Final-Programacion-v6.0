00470dc033edfa6ac2b595f5cbc698053b63f934
Aldlanga80 <aldo.d.langa@gmail.com>
2025-12-25 03:19:52 -0300

    fix externalapi

--- backend/src/controllers/externalControllers.ts
+++ backend/src/controllers/externalControllers.ts
@@ -3,9 +3,12 @@ import { fetchExternalData } from "../services/externalApiService";
 
 export const getExternal = async (req: Request, res: Response) => {
   try {
+    console.log("➡️ Llamando API externa...");
     const data = await fetchExternalData();
+    console.log("✅ Datos recibidos");
     res.json(data);
   } catch (error) {
+    console.error("❌ ERROR API EXTERNA:", error);
     res.status(500).json({ error: "Error al consultar API externa" });
   }
 };
