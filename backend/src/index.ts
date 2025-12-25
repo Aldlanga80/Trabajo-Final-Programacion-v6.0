96f2a7cb5d384e298bd12a7b2c22a9cfab3e9683
Aldlanga80 <aldo.d.langa@gmail.com>
2025-12-25 11:17:37 -0300

    authRoutes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,19 +3,28 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/mongodb"
 import productRouter from "./routes/productRoutes"
+import authRouter from "./routes/authRouter"
+import mongoose from "mongoose"
 
 dotenv.config()
 
 const app = express()
+const PORT = process.env.PORT || 4000
 
-app.use(cors())
+app.use(cors({ origin: "http://localhost:4000" }));
 app.use(express.json())
 
 connectDB()
 
+app.use("/auth", authRouter)
 app.use("/products", productRouter)
 
-const PORT = process.env.PORT || 4000
+
+mongoose.connect(process.env.MONGO_URI!)
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch(err => console.error("Error DB:", err));
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
