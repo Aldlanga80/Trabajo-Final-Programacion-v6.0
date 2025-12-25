45a18220c3461de336781aa2212049f57b78dcdf
Aldlanga80 <aldo.d.langa@gmail.com>
2025-12-25 16:51:59 -0300

    configurando atlas

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  }
+  base: './' // <- importante para rutas relativas en producción
 });
