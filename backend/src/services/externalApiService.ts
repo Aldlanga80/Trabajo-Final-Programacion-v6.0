96fd436ce09e5c901f093eec417dbae7fe215f7e
Aldlanga80 <aldo.d.langa@gmail.com>
2025-12-25 03:46:05 -0300

    fix  cluodflare 403 fakestore

--- backend/src/services/externalApiService.ts
+++ backend/src/services/externalApiService.ts
@@ -1,6 +1,16 @@
 import axios from "axios";
 
 export const fetchExternalData = async () => {
-  const response = await axios.get("https://fakestoreapi.com/products");
+  const response = await axios.get(
+    "https://fakestoreapi.com/products",
+    {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "application/json",
+        "Referer": "https://fakestoreapi.com/"
+      }
+    }
+  );
+
   return response.data;
 };
