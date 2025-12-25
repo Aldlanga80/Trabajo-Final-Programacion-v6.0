d231ee21038b239f3e664bdd58badf05e53f423b
Aldlanga80 <aldo.d.langa@gmail.com>
2025-12-25 03:35:03 -0300

    fix duplicated index and external route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,21 @@
-import cors from "cors"
-import express from "express"
-import dotenv from "dotenv"
-import { connectDB } from "./config/mongodb"
-import externalRouter from "./routes/externalRoutes"
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/mongodb";
+import externalRouter from "./routes/externalRoutes";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-connectDB()
+connectDB();
 
-app.use("/api", externalRouter)
+app.use("/api", externalRouter);
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
