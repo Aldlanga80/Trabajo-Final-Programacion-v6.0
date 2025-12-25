f59769f4d5bb1239fe26653adc0a503d4c12d9df
Aldlanga80 <aldo.d.langa@gmail.com>
2025-12-25 02:48:04 -0300

    fix externalapi using axios

--- backend/src/services/externalApiService.ts
+++ backend/src/services/externalApiService.ts
@@ -1,11 +1,6 @@
-import fetch from "node-fetch";
+import axios from "axios";
 
 export const fetchExternalData = async () => {
-  const res = await fetch("https://fakestoreapi.com/products");
-
-  if (!res.ok) {
-    throw new Error("Error al obtener datos externos");
-  }
-
-  return await res.json();
+  const response = await axios.get("https://fakestoreapi.com/products");
+  return response.data;
 };
